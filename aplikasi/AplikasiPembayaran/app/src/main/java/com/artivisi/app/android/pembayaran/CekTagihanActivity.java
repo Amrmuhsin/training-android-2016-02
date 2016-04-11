71d7fb6f3c41929ec6cb1a43c7d362ad8407b807
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-11 11:26:47 +0700

    starter aplikasi android

---
+++ aplikasi/AplikasiPembayaran/app/src/main/java/com/artivisi/app/android/pembayaran/CekTagihanActivity.java
@@ -0,0 +1,13 @@
+package com.artivisi.app.android.pembayaran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CekTagihanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cek_tagihan);
+    }
+}
