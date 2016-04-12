ce375d1bf31d0173eaa51b5786d41f49bb5d5cc3
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-12 14:44:42 +0700

    aplikasi server

---
+++ aplikasi/pembayaran-server/src/main/java/com/artivisi/pembayaran/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.artivisi.pembayaran.controller;
+
+import com.artivisi.pembayaran.dto.LoginRequest;
+
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Map<String, Object> login(@RequestBody @Valid LoginRequest login){
+        String validUsername = "endy";
+        String validPassword = "123";
+
+        Map<String, Object> hasil = new HashMap<>();
+
+        if(validUsername.equals(login.getUsername()) && validPassword.equals(login.getPassword())){
+            hasil.put("success", true);
+        } else {
+            hasil.put("success", false);
+        }
+
+        return hasil;
+    }
+}
---
+++ aplikasi/pembayaran-server/src/test/java/com/artivisi/pembayaran/PembayaranServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.artivisi.pembayaran;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PembayaranServerApplication.class)
+@WebAppConfiguration
+public class PembayaranServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
