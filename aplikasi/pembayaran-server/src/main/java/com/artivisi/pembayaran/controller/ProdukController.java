7fea2ed5810a06a47b28eb2283e83ce341637037
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-13 15:22:16 +0700

    implement rest service untuk ambil data produk

---
+++ aplikasi/pembayaran-server/src/main/java/com/artivisi/pembayaran/controller/ProdukController.java
@@ -0,0 +1,22 @@
+package com.artivisi.pembayaran.controller;
+
+import com.artivisi.pembayaran.entity.Produk;
+import com.artivisi.pembayaran.service.PembayaranService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produk")
+public class ProdukController {
+    
+    @Autowired private PembayaranService pembayaranService;
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Page<Produk> semuaProduk(Pageable page){
+        return pembayaranService.semuaProduk(page);
+    }
+}
--- aplikasi/pembayaran-server/src/main/java/com/artivisi/pembayaran/service/PembayaranService.java
+++ aplikasi/pembayaran-server/src/main/java/com/artivisi/pembayaran/service/PembayaranService.java
@@ -3,16 +3,18 @@ package com.artivisi.pembayaran.service;
 import com.artivisi.pembayaran.dao.AntrianGcmDao;
 import com.artivisi.pembayaran.dao.ProdukDao;
 import com.artivisi.pembayaran.entity.Produk;
+import com.artivisi.pembayaran.entity.User;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service @Transactional
 public class PembayaranService {
     @Autowired private ProdukDao produkDao;
-    @Autowired private AntrianGcmDao antrianGcmDao;
     @Autowired private GcmService gcmService;
     
     public void simpan(Produk p){
@@ -21,5 +23,9 @@ public class PembayaranService {
         data.put("action", "update");
         data.put("content", "produk");
         gcmService.kirimGcmMessage("/topics/produk", data);
+    }
+
+    public Page<Produk> semuaProduk(Pageable page) {
+        return produkDao.findAll(page);
     }
 }
