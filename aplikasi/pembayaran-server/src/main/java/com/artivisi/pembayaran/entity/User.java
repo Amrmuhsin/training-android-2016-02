58d4783e5f7a929e76c99b7d63f5dba089b5db20
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-13 11:55:34 +0700

    konfigurasi koneksi database local

---
+++ aplikasi/pembayaran-server/src/main/java/com/artivisi/pembayaran/entity/User.java
@@ -0,0 +1,38 @@
+package com.artivisi.pembayaran.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "m_user")
+public class User extends BaseEntity {
+    
+    @NotNull @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @NotNull @NotEmpty
+    @Column(nullable = false)
+    private String fullname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+    
+    
+}
