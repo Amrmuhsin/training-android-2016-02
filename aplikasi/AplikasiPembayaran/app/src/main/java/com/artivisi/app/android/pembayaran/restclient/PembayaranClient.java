b9e7e7b559f1b5543c808e8d474ebeb6f6d8b2c5
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-14 17:22:47 +0700

    mengambil cookie dari server

--- aplikasi/AplikasiPembayaran/app/src/main/java/com/artivisi/app/android/pembayaran/restclient/PembayaranClient.java
+++ aplikasi/AplikasiPembayaran/app/src/main/java/com/artivisi/app/android/pembayaran/restclient/PembayaranClient.java
@@ -1,9 +1,16 @@
 package com.artivisi.app.android.pembayaran.restclient;
 
+import android.util.Log;
+
 import com.artivisi.app.android.pembayaran.dto.Page;
 import com.artivisi.app.android.pembayaran.dto.Produk;
 import com.artivisi.app.android.pembayaran.exception.GagalLoginException;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -15,26 +22,36 @@ import java.util.Map;
  * Created by endymuhardin on 4/12/16.
  */
 public class PembayaranClient {
+    private static final String TAG = "REST";
     private static final String URL_SERVER = "https://pembayaran.herokuapp.com";
+    private static final String URL_SERVER_PEGADAIAN = "http://sdm.pegadaian.co.id";
+    private String cookiePegadaian;
 
     private RestTemplate restTemplate = new RestTemplate();
 
     public void login(String username, String password) throws GagalLoginException {
-        Map<String, String> loginData = new HashMap<>();
-        loginData.put("username", username);
-        loginData.put("password", password);
+        MultiValueMap<String, String> loginData = new LinkedMultiValueMap<>();
+        loginData.add("username", username);
+        loginData.add("password", password);
 
-        Map<String, Object> hasil = restTemplate
-                .postForObject(URL_SERVER + "/api/login",
+        ResponseEntity<Map> responseEntity = restTemplate
+                .postForEntity(URL_SERVER_PEGADAIAN + "/api/userLogin.php",
                     loginData, Map.class, new Object[]{}
                 );
 
-        if(hasil == null){
-            throw new GagalLoginException("Response invalid");
+        HttpHeaders headers = responseEntity.getHeaders();
+        List<String> cookies = headers.get("Set-Cookie");
+
+        for(String cookie : cookies){
+            Log.d(TAG, "Cookie Header : "+cookie);
+            cookiePegadaian = cookie.substring(0,(cookie.indexOf(";")+1));
+            Log.d(TAG, "Cookie setelah substring : "+cookiePegadaian);
         }
 
-        if(!(Boolean)hasil.get("success")){
-            throw new GagalLoginException("Username / Password salah");
+        Map<String, Object> hasil = responseEntity.getBody();
+
+        if(hasil.get("username") == null){
+            throw new GagalLoginException("Response invalid");
         }
     }
 
